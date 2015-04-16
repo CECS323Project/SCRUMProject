e71acdf2f53a94ad4b34460a7ab9c8fecf9bb643
Russell Tan <sell_nat@yahoo.com>
2015-04-16 11:03:18 -0700

    Now have basic select for sprints, no db connection yet

--- JavaProject/src/JDBCConnections.java
+++ JavaProject/src/JDBCConnections.java
@@ -1,4 +1,42 @@
+import java.sql.*;
+import java.util.Scanner;
 public class JDBCConnections 
 {
+	private String dbUrl = "";
+	private Connection conn = null;
+	private Statement stmnt = null;
+	private String uName = "";
+	private String pass = "";
+	private Scanner userIn = new Scanner(System.in);
+	
+	public Connection getConnection()
+	{
+		try
+		{
+			conn = DriverManager.getConnection(dbUrl,uName,pass);
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		 return conn;
+	}
+	
+	public Statement getStatment()
+	{
+		return stmnt;
+	}
+
+	public void setStatment(Connection stmntConn) 
+	{
+		try 
+		{
+			stmnt = stmntConn.createStatement();
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
 
 }
--- JavaProject/src/Sprints.java
+++ JavaProject/src/Sprints.java
@@ -1,6 +1,12 @@
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
-public class Sprints 
+
+public class Sprints
 {
+	private JDBCConnections jdbcConn = new JDBCConnections();
 	public void showAll()
 	{
 		System.out.println("Here is a list of all sprints:\n");
@@ -9,6 +15,44 @@ public class Sprints
 
 	private void getAllSprints() 
 	{
+		try
+		{
+			int k = 0;
+			String sprint;
+			jdbcConn.setStatment(jdbcConn.getConnection());
+			ResultSet results = jdbcConn.getStatment().executeQuery("select * from Sprints");
+			ResultSetMetaData rsmd = results.getMetaData();
+			int cols = rsmd.getColumnCount();
+			
+			for(int i = 1; k <= cols; i++)
+			{
+				System.out.println(rsmd.getColumnLabel(i)+"\t");
+			}
+			
+			System.out.println("\n-----------------------------------------------------------");
+			
+			while(results.next())
+			{
+				k++;
+				String sprintID = results.getString("SprintID");
+				String backlogID = results.getString("BackLogID");
+				String date = results.getString("Date").toString();
+				String teamID = results.getString("TeamID");
+				
+				System.out.format("%8s,%8s,%8s,%8s",sprintID,backlogID,date,teamID);
+			}
+			
+			System.out.println("\n");
+			results.close();
+			rsmd = null;
+			jdbcConn.getStatment().close();
+			
+			
+		}
+		catch(SQLException e)
+		{
+			
+		}
 		//sql select * from sprints
 		
 	}
